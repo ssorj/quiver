3515d0fae8e61b18c35113f5ad8a806928b50252
Justin Ross <jross@apache.org>
2016-09-17 08:55:20 -0700

    Add a qpid-messaging-cpp impl; add some utilities for bouncing brokr queues

--- exec/quiver-qpid-messaging-cpp.cpp
+++ exec/quiver-qpid-messaging-cpp.cpp
@@ -19,24 +19,117 @@
  *
  */
 
+#include <qpid/messaging/Connection.h>
+#include <qpid/messaging/Message.h>
+#include <qpid/messaging/Receiver.h>
+#include <qpid/messaging/Sender.h>
+#include <qpid/messaging/Session.h>
+
 #include <iostream>
+#include <chrono>
+
+using namespace qpid::messaging;
+using namespace qpid::types;
+using namespace std::chrono;
+
+long now() {
+    return duration_cast<milliseconds>
+        (system_clock::now().time_since_epoch()).count();
+}
+
+struct Client {
+    std::string output_dir;
+    std::string domain;
+    std::string path;
+    std::string operation;
+    int messages;
+    int bytes;
+    int credit;
+
+    int transfers = 0;
+
+    void run();
+    void sendMessages(Session&);
+    void receiveMessages(Session&);
+};
+
+void Client::run() {
+    Connection conn(domain, "{protocol: amqp1.0, sasl_mechanisms: ANONYMOUS}");
+    conn.open();
+
+    try {
+        Session session = conn.createSession();
+
+        if (operation == "send") {
+            sendMessages(session);
+        } else if (operation == "receive") {
+            receiveMessages(session);
+        } else {
+            throw std::exception();
+        }
+    } catch (const std::exception& e) {
+        conn.close();
+        throw;
+    }
+}
+
+void Client::sendMessages(Session& session) {
+    Sender sender = session.createSender(path);
+    sender.setCapacity(credit);
+
+    std::string body(bytes, 'x');
+    
+    while (transfers < messages) {
+        Message message(body);
+        message.setMessageId(std::to_string(transfers + 1));
+        message.setProperty("SendTime", Variant(now()));
+
+        sender.send(message);
+            
+        transfers++;
+    }
+}
+
+void Client::receiveMessages(Session& session) {
+    Receiver receiver = session.createReceiver(path);
+    receiver.setCapacity(credit);
+
+    Message message;
+
+    while (transfers < messages) {
+        if (receiver.getAvailable() == 0) {
+            continue;
+        }
+
+        receiver.get(message);
+        session.acknowledge();
+
+        std::string id = message.getMessageId();
+        long stime = message.getProperties()["SendTime"];
+        long rtime = now();
+
+        std::cout << id << "," << stime << "," << rtime << "\n";
+        
+        transfers++;
+    }
+}
 
 int main(int argc, char** argv) {
-    std::string output_dir = argv[1];
     std::string mode = argv[2];
-    std::string domain = argv[4];
-    std::string path = argv[5];
-    std::string operation = argv[3];
-    int messages = std::atoi(argv[6]);
-    int bytes = std::atoi(argv[7]);
-    int credit = std::atoi(argv[8]);
 
-    std::cerr << output_dir << std::endl;
-    std::cerr << mode << std::endl;
-    std::cerr << domain << std::endl;
-    std::cerr << path << std::endl;
-    std::cerr << operation << std::endl;
-    std::cerr << messages << std::endl;
-    std::cerr << bytes << std::endl;
-    std::cerr << credit << std::endl;
+    Client client;
+    client.output_dir = argv[1];
+    client.domain = argv[3];
+    client.path = argv[4];
+    client.operation = argv[5];
+    client.messages = std::atoi(argv[6]);
+    client.bytes = std::atoi(argv[7]);
+    client.credit = std::atoi(argv[8]);
+
+    try {
+        client.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 }
