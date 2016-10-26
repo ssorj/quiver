62190ddf1413cd23cecc5e208339a8b16d9a941e
Justin Ross <jross@apache.org>
2016-10-26 06:49:37 -0700

    Turn on proton cpp builds if the library is present

--- misc/local-libs-env.sh
+++ misc/local-libs-env.sh
@@ -2,3 +2,7 @@ export LIBRARY_PATH=$HOME/.local/lib64:/usr/local/lib64
 export C_INCLUDE_PATH=$HOME/.local/include:/usr/local/include
 export CPLUS_INCLUDE_PATH=$C_INCLUDE_PATH
 export LD_LIBRARY_PATH=$LIBRARY_PATH
+
+if [[ -f $HOME/.local/lib64/libqpid-proton-cpp.so || -f /usr/local/lib64/libqpid-proton-cpp.so ]]; then
+    export QPID_PROTON_CPP_ENABLED=1
+fi
