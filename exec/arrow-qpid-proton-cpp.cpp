88def3bdb47b862d84606c81a16951e1e9864670
Justin Ross <jross@apache.org>
2016-11-01 10:03:09 -0700

    Set the jms message type in the cpp impl

--- exec/arrow-qpid-proton-cpp.cpp
+++ exec/arrow-qpid-proton-cpp.cpp
@@ -103,8 +103,7 @@ struct handler : public proton::messaging_handler {
             proton::message m(body);
             m.id(id);
             m.properties().put("SendTime", stime);
-            //m.content_type("application/octet-stream");
-            //m.message_annotations().put("x-opt-jms-msg-type", 3);
+            m.message_annotations().put("x-opt-jms-msg-type", 3);
 
             s.send(m);
             sent++;
