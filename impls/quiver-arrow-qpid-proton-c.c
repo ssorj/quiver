ssorj/quiver

commit e1e8cf90d49f8f3161d35872294550c17d795439

qpid-proton-c arrow: Configure pn_connection_set_hostname in order that TLS connections may traverse a HA proxies etc (i.e. SNI) The other arrows already seem to this this.

impls/quiver-arrow-qpid-proton-c.c
@@ -247,6 +247,10 @@ static bool handle(struct arrow* a, pn_event_t* e) {
                 pn_connection_set_password(a->connection, a->password);
             }
 
+            char domain[strlen(a->host)  + (a->port ? (strlen(a->port) + 1) : 0) + 1];
+            sprintf(domain, (a->port ? "%s:%d" : "%s"), a->host, a->port);
+            pn_connection_set_hostname(a->connection, domain);
+
             pn_session_t* ssn = pn_session(pn_event_connection(e));
             pn_session_open(ssn);
             pn_link_t* l = NULL;

commit a69298c905326b534c60502cb18780adb7a8d6d7

fix segmentation fault introduced by e1e8cf90

impls/quiver-arrow-qpid-proton-c.c
@@ -241,15 +241,18 @@ static bool handle(struct arrow* a, pn_event_t* e) {
         pn_connection_set_container(pn_event_connection(e), a->id);
         if (a->channel_mode == ACTIVE) {
             if (a->username) {
-                pn_connection_set_user(a->connection, a->username);
+                pn_connection_set_user(pn_event_connection(e), a->username);
             }
             if (a->password) {
-                pn_connection_set_password(a->connection, a->password);
+                pn_connection_set_password(pn_event_connection(e), a->password);
             }
 
-            char domain[strlen(a->host)  + (a->port ? (strlen(a->port) + 1) : 0) + 1];
-            sprintf(domain, (a->port ? "%s:%d" : "%s"), a->host, a->port);
-            pn_connection_set_hostname(a->connection, domain);
+            if (a->host) {
+                bool has_port = a->port && strlen(a->port) > 0;
+                char domain[strlen(a->host) + (has_port ? (strlen(a->port) + 1) : 0) + 1];
+                sprintf(domain, (has_port ? "%s:%s" : "%s"), a->host, a->port);
+                pn_connection_set_hostname(pn_event_connection(e), domain);
+            }
 
             pn_session_t* ssn = pn_session(pn_event_connection(e));
             pn_session_open(ssn);