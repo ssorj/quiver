c0bb964b8a29234295f500b7c1458cfe66f7c8da
Jiri Danek <jdanek@redhat.com>
2020-07-24 12:19:40 +0200

    fixup Fortify devel.sh; run from different dir, turn on safer bash

--- devel.sh
+++ devel.sh
@@ -4,5 +4,5 @@ set -Eeuxo pipefail
 BASEDIR=$(readlink -f "$(dirname "$0")")
 export QUIVER_HOME=$BASEDIR/build/quiver
 export PATH=$PWD/build/bin:$BASEDIR/scripts:$PATH
-export PYTHONPATH=$QUIVER_HOME/python:$BASEDIR/python${$PYTHONPATH:+:${PYTHONPATH}}
-export NODE_PATH=/usr/lib/node_modules${$NODE_PATH:+:${NODE_PATH}}
+export PYTHONPATH=$QUIVER_HOME/python:$BASEDIR/python${PYTHONPATH:+:${PYTHONPATH}}
+export NODE_PATH=/usr/lib/node_modules${NODE_PATH:+:${NODE_PATH}}
