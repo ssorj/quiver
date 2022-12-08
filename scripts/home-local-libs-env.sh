85c229901ab2fb84f35c4a5c65476db6d37026f8
Justin Ross <jross@apache.org>
2022-12-08 07:27:50 -0500

    Improve and simplify my env scripts

--- scripts/home-local-libs-env.sh
+++ scripts/home-local-libs-env.sh
@@ -1,9 +1,5 @@
-export LIBRARY_PATH=$HOME/.local/lib64:/usr/local/lib64
-export C_INCLUDE_PATH=$HOME/.local/include:/usr/local/include
+export LIBRARY_PATH=$HOME/.local/lib64
+export C_INCLUDE_PATH=$HOME/.local/include
 export CPLUS_INCLUDE_PATH=$C_INCLUDE_PATH
 export LD_LIBRARY_PATH=$LIBRARY_PATH
-export PYTHONPATH=$HOME/.local/lib/python3.9/site-packages:$HOME/.local/lib64/python3.9/site-packages:$PYTHONPATH
-
-# if [[ -f $HOME/.local/lib64/libqpid-proton-cpp.so || -f /usr/local/lib64/libqpid-proton-cpp.so ]]; then
-#     export QPID_PROTON_CPP_ENABLED=1
-# fi
+export PYTHONPATH=$(python -m site --user-site)
